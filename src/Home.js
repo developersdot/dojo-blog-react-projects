5aef75743b6ebc6ae05b9944255cf98ea6ed13dd
abdullahjaved <aj.abdullahjaved@gmail.com>
2021-12-06 21:59:43 +0500

    statis blocks

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My New Website', body: 'lorem ipsum ...', author: 'mario', id: 1 },
+        { title: 'Welcome Party', body: 'lorem ipsum ...', author: 'yoshi', id: 2 },
+        { title: 'Web Dev Top Tip', body: 'lorem ipsum ...', author: 'mario', id: 3 }
+    ]);
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+        {
+            blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2> { blog.title } </h2>
+                    <p>Written By { blog.author }</p>
+                </div>
+                
+            
+            ))
+        }
         </div>
     );
 }
